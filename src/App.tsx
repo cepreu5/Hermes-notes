36ea4e808e2517b8857f0ef84fafa5e163242c76
cepreu5 <31445705+cepreu5@users.noreply.github.com>
2026-09-10 13:51:12 +0300

    add /share/:token route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { DefaultProviders } from "./components/providers/default.tsx";
 import AuthCallback from "./pages/auth/Callback.tsx";
 import NotesApp from "./pages/notes/page.tsx";
+import SharedNotePage from "./pages/shared/page.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import { useServiceWorker } from "@/hooks/use-service-worker.ts";
 
 export default function App() {
-    useServiceWorker();  return (
+  useServiceWorker();
+  return (
     <DefaultProviders>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<NotesApp />} />
+          <Route path="/share/:token" element={<SharedNotePage />} />
           <Route path="/auth/callback" element={<AuthCallback />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
